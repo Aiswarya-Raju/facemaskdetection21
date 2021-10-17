1a19b5c754530021f763a9dae668db90364d82dd
Aiswarya-Raju <83088089+Aiswarya-Raju@users.noreply.github.com>
2021-10-17 23:41:29 +0530

    added app

--- register_login/settings.py
+++ register_login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hphj@puavsjvdn@b+g1)=2l-pbg)sy3-jqet$_5ouqgn0$dvx-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','facemaskdetection21.herokuapp.com']
 
 
 # Application definition
